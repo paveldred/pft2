2c99835c099fb11f5c92faad54c1e360f7cc73c2
Pavel Smolkov <smolcov@bk.ru>
2019-05-07 18:32:29 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft2/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft2/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft2.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
